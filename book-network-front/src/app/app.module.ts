993ef7e0a3841bafaa7c6540a880b7c793165b14
Subhi YD <subhy809fr@gmail.com>
2024-06-22 22:25:20 +0200

    v1.2.7 frontend: upgrade angular version upgraded to 18

--- book-network-front/src/app/app.module.ts
+++ book-network-front/src/app/app.module.ts
@@ -3,30 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { LoginComponent } from './components/login/login.component';
 import {FormsModule} from "@angular/forms";
 import { SignupComponent } from './components/signup/signup.component';
 import { ActivateAccountComponent } from './components/activate-account/activate-account.component';
 import { CodeInputModule } from "angular-code-input";
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent,
-    ActivateAccountComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    CodeInputModule
-  ],
-  providers: [
-    HttpClient
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+        SignupComponent,
+        ActivateAccountComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        CodeInputModule], providers: [
+        HttpClient,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
